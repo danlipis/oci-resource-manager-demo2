ef51e6a66bd98b7ed0cde3a778d7c41484492fca
Daniel dos Santos Bastos <daniel.cgid@gmail.com>
2021-09-15 11:35:31 -0300

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -17,6 +17,6 @@ resource "oci_objectstorage_bucket" "create_bucket" {
     access_type = "ObjectRead"
 }
 
-output "new_bucket2" {
+output "new_bucket" {
   value = oci_objectstorage_bucket.create_bucket
 }
